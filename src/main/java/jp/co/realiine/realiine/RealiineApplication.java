48b86c6d4f90dc1a3ae1a874563c2c8b24b376bf
shinichiurakawa <shinichi.urakawa@gmail.com>
2018-12-15 19:44:39 +0900

    first commit

---
+++ src/main/java/jp/co/realiine/realiine/RealiineApplication.java
@@ -0,0 +1,14 @@
+package jp.co.realiine.realiine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealiineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealiineApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/jp/co/realiine/realiine/RealiineApplicationTests.java
@@ -0,0 +1,17 @@
+package jp.co.realiine.realiine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealiineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
